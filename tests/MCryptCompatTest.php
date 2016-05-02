67d04c34ec9bb92c67889e3b00267c4ba6f9bf9c
terrafrost <terrafrost@php.net>
2016-05-02 11:11:13 -0500

    add more unit tests

--- tests/MCryptCompatTest.php
+++ tests/MCryptCompatTest.php
@@ -12,4 +12,27 @@ class MCryptCompatTest extends PHPUnit_Framework_TestCase
         $compat = bin2hex(phpseclib_mcrypt_encrypt('rijndael-128', str_repeat('a', 16), 'asdf', 'cbc', str_repeat('a', 16)));
         $this->assertEquals($mcrypt, $compat);
     }
+
+    public function testListAlgorithms()
+    {
+        $this->assertInternalType('array', phpseclib_mcrypt_list_algorithms());
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testBadParamsMcrypt()
+    {
+        mcrypt_encrypt('rijndael-128', 'abc', 'asdf', 'cbc', 'zz');
+    }
+
+    /**
+     * pretty much the same thing as testBadParamsMcrypt
+     *
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testBadParamsMcryptCompat()
+    {
+        phpseclib_mcrypt_encrypt('rijndael-128', 'abc', 'asdf', 'cbc', 'zz');
+    }
 }
